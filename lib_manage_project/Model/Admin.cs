c533890c3c0faabba0c9de81ab9d75c3a2c87bcb
Hossam_Hassan <Hossam_Hassan>
2020-04-05 04:55:17 +0200

    Create Classes based on Database

---
+++ lib_manage_project/Model/Admin.cs
@@ -0,0 +1,13 @@
+
+
+namespace lib_manage_project.Model
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class Admin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
