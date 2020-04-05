80f25308d89a3799d23498ecf88c4668f682037d
akram ebrahim <akramebrahim99@yahoo.com>
2020-04-05 05:48:15 +0200

    make a Validation on Classes

--- lib_manage_project/Model/Author.cs
+++ lib_manage_project/Model/Author.cs
@@ -3,7 +3,8 @@ namespace lib_manage_project.Model
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Author
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -11,8 +12,12 @@ namespace lib_manage_project.Model
         {
             this.Books = new HashSet<Book>();
         }
-    
+
+        
         public int Author_Id { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression(".{10,50}")]
         public string Author_Name { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- lib_manage_project/Model/Client.cs
+++ lib_manage_project/Model/Client.cs
@@ -2,7 +2,8 @@ namespace lib_manage_project.Model
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Client
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -12,11 +13,33 @@ namespace lib_manage_project.Model
         }
     
         public int Client_Id { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression(".{1,50}")]
         public string Client_Name { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression(".{6,20}")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression(".{8,50}")]
+        [Phone]
         public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression(".{10,50}")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [RegularExpression("[0-9]{10,50}")]
         public string National_Id { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [DataType(DataType.Date)]
+        [NotInFuture]
         public System.DateTime DateOfBirth { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
