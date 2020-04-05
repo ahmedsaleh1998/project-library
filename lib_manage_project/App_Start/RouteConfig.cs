6a027373570c6b858b797bce4221616ef8b631fb
Hossam_Hassan <Hossam_Hassan>
2020-04-05 06:40:24 +0200

    Create Admin Home Page

--- lib_manage_project/App_Start/RouteConfig.cs
+++ lib_manage_project/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace lib_manage_project
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "AdminHomePage", id = UrlParameter.Optional }
             );
         }
     }
